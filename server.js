dfdf6a3dab8077496abaca404f74e0c6d2fa4361
Caspar Skripkauskas <caspar@completewithus.com>
2018-06-14 15:33:46 +0300

    updated for heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackConf = require('./webpack.config.js');
+  app.use(webpackMiddleware(webpack(webpackConf)));
+}else{
+  app.use(express.static(path.join(__dirname, 'dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('server is up'));
